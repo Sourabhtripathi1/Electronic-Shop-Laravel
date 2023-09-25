c6634c207ad08cd6efd0abbf3cf09db99ae11972
Sourabhtripathi1 <ganeshprasadtripathi38@gmail.com>
2023-09-25 19:31:39 +0530

    push

--- resources/views/admin/EditProduct.blade.php
+++ resources/views/admin/EditProduct.blade.php
@@ -92,7 +92,22 @@ if ($x->Brand_id==$prod['Brand']) {
                         <td>{{ $v['Color'] }}</td>
                         <td>{{ $v['Stock'] }}</td>
                         <td>{{ $v['Price'] }}</td>
-                        <td></td>
+                        <td>
+                            @php
+                                
+                                $check = json_decode($v['Picture']);
+                                
+                                $x = array_filter($pics, function ($pic) use ($check) {
+                                    if (in_array($pic['Picture_id'], $check)) {
+                                        return $pic;
+                                    }
+                                });
+                                
+                            @endphp
+
+                            {{ count($x) }}
+
+                        </td>
                         <td>
                             <div class="d-flex">
                                 <div class="mx-1">
@@ -111,8 +126,9 @@ if ($x->Brand_id==$prod['Brand']) {
                                     </a>
                                 </div>
                                 <div class="mx-1">
-                                    <a href="">
-                                        <button class="btn btn-primary btn-sm">
+                               
+                                        <button class="btn btn-primary btn-sm"  data-toggle="modal"
+                                        data-target="#Modal{{ $v['variant_id'] }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
                                                 <style>
                                                     svg {
@@ -123,10 +139,62 @@ if ($x->Brand_id==$prod['Brand']) {
                                                     d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
                                             </svg>
                                         </button>
-                                    </a>
+
+
+                                        <div class="modal fade" id="Modal{{ $v['variant_id'] }}" tabindex="999"
+                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered modal-lg">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <form action=""
+                                                method="post" enctype="multipart/form-data">
+                                                <div class="modal-body">
+                                                    @csrf
+                                                    @method('PUT')
+                                                    
+                                                    <div class="container">
+                                                        <div class="mb-3">
+                                                            <label for="" class="form-label">Color</label>
+                                                            <input type="text" class="form-control" name="color" value="{{ $v['Color'] }}">
+                                                        </div>
+    
+                                                        <div class="mb-3">
+                                                            <label for="" class="form-label">Stock</label>
+                                                            <input type="text" class="form-control" name="stock" value="{{ $v['Stock'] }}">
+                                                        </div>
+    
+                                                        <div class="mb-3">
+                                                            <label for="" class="form-label">Price</label>
+                                                            <input type="text" class="form-control" name="price" value="{{ $v['Price'] }}">
+                                                        </div>
+                                                    </div>
+
+                                                    @foreach($x as $p)
+
+                                                    <img height="100px" width="150px" src="{{ asset('/storage/site-assets/') }}/{{$p['Source']}}">
+                                                    <br><br><br>
+                                                    @endforeach
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary">Update
+                                                    </button>
+                                                </div>
+                                            </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                
                                 </div>
 
-                                <form  method="post">
+                                <form method="post">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-danger btn-sm">
