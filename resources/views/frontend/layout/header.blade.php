f125b5ad3d44b0389ddf811446a50d2568655165
Sourabhtripathi1 <ganeshprasadtripathi@gmail.com>
2023-11-09 22:42:54 +0530

    push

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -48,13 +48,12 @@
         <!-- TOP HEADER -->
         <div id="top-header">
             <div class="container">
-                <ul class="header-links pull-left">
+                <ul class="header-links pull-left header-links-pull-left">
                     <li><a href="#"><i class="fa fa-phone"></i>+91 8619826308</a></li>
                     <li><a href="#"><i class="fa fa-envelope-o"></i> sourabhtripathi8103@gmail.com</a></li>
 
                 </ul>
                 <ul class="header-links pull-right">
-                    {{-- <li><a href="#"><i class="fa fa-dollar"></i> USD</a></li> --}}
                     <li><a href="{{env('APP_URL')}}/user/dashboard"><i class="fa fa-user-o"></i> My Account</a></li>
                 </ul>
             </div>
